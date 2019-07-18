b07c21c850d1326a8ccf010bdf1fd32d11310555
S Shahrukh <37923161+shahrukhsyed@users.noreply.github.com>
2019-07-18 15:10:44 +0530

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,21 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app',
+    templateUrl: 'app.component.html'
+})
+
+export class AppComponent {
+
+     name:string="";
+    address:string="";
+    pincode:number;
+    status:string="INVALID";
+
+
+    getData(){
+
+        this.status="VALID";
+
+    }
+ }
